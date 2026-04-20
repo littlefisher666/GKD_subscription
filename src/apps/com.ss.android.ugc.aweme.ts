b6d8d71a1f34a13ea732a9c3ef09cd02afe2a09d
jinyanan <jinyanan@hdzk.cn>
2026-04-20 11:43:15 +0800

    fix(com.ss.android.ugc.aweme): 优化返回规则逻辑，将 swipe 改为 back

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -326,12 +326,13 @@ export default defineGkdApp({
         {
           key: 1,
           fastQuery: true,
-          action: 'swipe',
-          swipeArg: {
-            start: { x: 'screenWidth * 0.2', y: 'screenHeight * 0.8' },
-            end: { x: 'screenWidth * 0.2', y: 'screenHeight * 0.7' },
-            duration: 50,
-          },
+          action: 'back',
+          // action: 'swipe',
+          // swipeArg: {
+          //   start: { x: 'screenWidth * 0.2', y: 'screenHeight * 0.8' },
+          //   end: { x: 'screenWidth * 0.2', y: 'screenHeight * 0.7' },
+          //   duration: 50,
+          // },
           activityIds: '.main.MainActivity',
           matches: [
             '[desc^="已选中"] > [text="推荐"][visibleToUser=true]', // 其他页面可能会误触回到推荐页
